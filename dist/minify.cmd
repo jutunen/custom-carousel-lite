36b4c28be145bf27eeaad8ada523fc5dfc4510aa
jussi <Jussi Utunen>
2020-07-11 12:44:19 +0300

    initial minify.cmd commit

---
+++ dist/minify.cmd
@@ -0,0 +1,1 @@
+terser wc-carousel-lite.js -c -m --mangle-props reserved=[constructor,observedAttributes,disconnectedCallback,attributeChangedCallback,connectedCallback,Customcarousel,startAutoplay,stopAutoplay,next,prev,gotoIndex] -o wc-carousel-lite.min.js
